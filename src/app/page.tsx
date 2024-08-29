a76e05eae35f19da450e0b55c62647a8b6eb65fc
aigradschool Gist <aigradschoolgist@aigradschoolui-iMac-2.local>
2024-08-29 22:02:06 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client' 
 import React, {useState, useEffect } from 'react';
-import recoder  from "@/data/recoder-0";
-import dslSol from '@/data/solver-dsl-0';
-import final_result from "@/data/final_result";
-import dbscan_result from "@/data/dbscan_result";
-import DropDownSelect from '@/components/DropDownSelector';
-import FancyList from "@/components/FancyList";
-import BitmapRender from '@/components/BitmapRender';
+import recoder  from "../data/recoder-0.json";
+import dslSol from '../data/solver-dsl-0.json';
+import final_result from "../data/final_result.json";
+import dbscan_result from "../data/dbscan_result.json";
+import DropDownSelect from '../components/DropDownSelector';
+import FancyList from "../components/FancyList";
+import BitmapRender from '../components/BitmapRender';
 
 
 
@@ -48,7 +48,7 @@ function Home () {
           [2, 3, 4, 0, 1],
           [3, 4, 0, 1, 2],
         ];
-	const takeId = (id) => id.slice(6,);
+	const takeId = (id: String) => id.slice(6,);
 	useEffect(()=>{
 		const data = dbscanData[dbscanIdx].map((idx)=> {
 		  return recoder[takeId(idx)]
@@ -62,7 +62,7 @@ function Home () {
 
 		const data = await curQAs.map((rData) => {
 				  const result =  rData.each_recoder.map((bData) => {
-				       return {id: rData.id, data: bData, dsl: dslSol["solve_"+rData.id].map(dslSingle=><p>{dslSingle}</p> )}
+				       return {id: rData.id, data: bData, dsl: dslSol["solve_"+rData.id].map((dslSingle,idx)=><p key={idx}>{dslSingle}</p> )}
 	//			       (<div>
 	//			         <BitmapRender data={bData.Q} />
 	//			         <BitmapRender data={bData.A} />
@@ -84,9 +84,9 @@ function Home () {
                          <FancyList items={callCluster} />
                        </div>
 		       <div className="flex">
-		       {  vis &&vis.map((data)=>{
+		       {  vis &&vis.map((data,idx)=>{
 	                 return (
-				 <div className="m-4">
+				 <div key={idx} className="m-4">
 				 <p>{data.id}</p>
 			         <BitmapRender data={data.data?.Q} />
 		         	 <BitmapRender data={data.data?.A} />
